883e81755f091614a68a84ff81270bd39c98b0f3
yitengtang <104777271+yitengtang@users.noreply.github.com>
2022-11-24 17:07:18 -0500

    Add files via upload

---
+++ ENUM/enumTest/enumTest/Program.cs
@@ -0,0 +1,14 @@
+﻿enum orientation : byte
+{
+    north = 1,
+    south =2,
+    east =3,
+    west = 4
+}
+
+orientation myDirection = orientation.north;
+
+Console.WriteLine("My direction is {0}", myDirection);
+
+Console.ReadKey();
+
---
+++ ENUM/enumTest/test/EnumTest/EnumTest/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace EnumTest;
+
+enum Orientation : byte
+{
+    north =1,
+    south = 2,
+    east =3,
+    west =4
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+
+        byte directionByte;
+        string directionString;
+
+        Orientation myDirection = Orientation.north;
+
+        directionByte = (byte)myDirection;
+        directionString = Convert.ToString(myDirection);
+
+        Console.WriteLine("My orientation is {0}", myDirection);
+        Console.WriteLine("byte equivalent = {0}", directionByte);
+        Console.WriteLine("string equivalent ={0}", directionString);
+        Console.ReadKey();
+    }
+}
+
