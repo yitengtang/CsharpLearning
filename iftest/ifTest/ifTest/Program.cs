b203712078c0307fbe44b509bbf044cf13ddfe46
yitengtang <104777271+yitengtang@users.noreply.github.com>
2022-11-24 17:08:12 -0500

    Add files via upload

---
+++ iftest/ifTest/ifTest/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string comparison;
+
+Console.WriteLine("Enter a number: ");
+
+double var1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ener another number: ");
+
+double var2 = Convert.ToDouble(Console.ReadLine());
+
+if (var1 < var2)
+
+    comparison = "less than";
+
+else
+{
+    if (var1 == var2)
+        comparison = "equal to";
+    else
+        comparison = "greater than";
+}
+
+Console.WriteLine(" ther first number is {0} the second number.", comparison );
+
+Console.ReadKey();
+
+
+
+
