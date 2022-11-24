8d8df4ef6b0e6ab4dbb713c6eeca8a2993f949dd
yitengtang <104777271+yitengtang@users.noreply.github.com>
2022-11-24 17:05:17 -0500

    Add files via upload

---
+++ BoolStudy/BoolStudy/BoolStudy/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Enter an integer: ");
+
+int myInt = Convert.ToInt32(Console.ReadLine());
+
+bool isLessThan10 = myInt < 10;
+
+bool isBetween0And5 = (0 <= myInt) && (myInt <= 5);
+
+Console.WriteLine("Integer less than 10? {0}", isLessThan10);
+
+Console.WriteLine("Integer is between 0 and 5? {0}", isBetween0And5);
+
+Console.WriteLine("Ecactly one of the above is true? {0}", isBetween0And5^isLessThan10); //if one of the bool value is true then it is true
+
+Console.ReadKey();
---
+++ doCycle/doCycle/doCycle/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+double balance, interestRate, targetBalance;
+
+Console.WriteLine("What is your current balance? ");
+
+balance = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("What is your current annual interest rat (in %)?");
+
+interestRate = 1 + Convert.ToDouble(Console.ReadLine()) / 100.0;
+
+Console.WriteLine("What balance you would to have?");
+
+targetBalance = Convert.ToDouble(Console.ReadLine());
+
+int totalYears = 0;
+
+do
+{
+    balance *= interestRate;
+    ++totalYears;
+}
+while (balance < targetBalance);
+
+Console.WriteLine("In {0} years {1} you will have a balance of {2}", totalYears,totalYears==1?"":"s", balance);
+
+Console.ReadKey();
+
+
+
