abbf9125b62b13ea04bf45dd688946d8e86aba48
yitengtang <104777271+yitengtang@users.noreply.github.com>
2022-11-24 17:04:15 -0500

    Add files via upload

---
+++ arrayTest/arrayTest/arrayTest/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string[] friendName = { "Robert Barwell", "Mike Parry", "Jeremy Beacock" };
+
+int i;
+Console.WriteLine("Here are {0} of my friends:", friendName.Length);
+
+for (i=0; i<friendName.Length; i++)
+{
+    Console.WriteLine(friendName[i]);
+}
+Console.ReadKey();
