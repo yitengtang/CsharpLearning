f0780ba7265a0ec7bb0de1463d97bc5a5725ecf6
yitengtang <104777271+yitengtang@users.noreply.github.com>
2022-11-24 17:08:58 -0500

    Add files via upload

---
+++ switchTest/switchTest/switchTest/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+const string myName = "karli";
+
+const string sexyName = "angelina";
+
+const string sillyName = "ploppy";
+
+string name;
+
+Console.WriteLine("What is your name? ");
+
+name = Console.ReadLine();
+
+switch (name.ToLower())
+{
+    case myName:
+        Console.WriteLine("Your have the same name as me!");
+        break;
+    case sexyName:
+        Console.WriteLine("My what a sexy name you have!");
+        break;
+    case sillyName:
+        Console.WriteLine("That's a very silly name.");
+        break;
+}
+
+Console.WriteLine("Hello, {0}", name);
+
+Console.ReadKey();
+
+
+
---
+++ typeConvert/typeConvert/typeConvert/Program.cs
@@ -0,0 +1,35 @@
+﻿short shortResults, shortVal = 4;
+
+int integerVal = 67;
+
+long longResult;
+
+float floatVal = 10.5F;
+
+double doubleResult, doubleVal = 99.999;
+
+string stringReslt, stringVal = "17";
+
+bool boolVal = true;
+
+Console.WriteLine("Variable Conversion Example");
+
+doubleResult = floatVal * shortVal;
+Console.WriteLine("Implicit, -> double: {0} * {1} -> {2}", floatVal, shortVal, doubleResult);
+
+shortResults = (short)floatVal;
+Console.WriteLine("Explicit, -> short: {0} -> {1}", floatVal, shortResults);
+
+stringReslt = Convert.ToString(boolVal) + Convert.ToString(doubleVal);
+Console.WriteLine("Explicit, -> string: \"{0}\" + \"{1}\" -> {2}", boolVal, doubleVal, stringReslt);
+
+longResult = integerVal + Convert.ToInt32(stringVal);
+Console.WriteLine("Mixed, ->long: {0} + {1} -> {2}", integerVal, stringVal, longResult);
+
+Console.ReadKey();
+
+
+
+
+
+
