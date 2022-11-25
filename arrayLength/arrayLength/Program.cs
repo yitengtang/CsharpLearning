0873971b3be54941c9ca976b65af227452fe47be
yitengtang <104777271+yitengtang@users.noreply.github.com>
2022-11-25 16:13:55 -0500

    Add files via upload

---
+++ arrayLength/arrayLength/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] intArray = { 1, 2, 3, 4, 5, 6 };
+
+int intLength;
+
+intLength = intArray.Length;
+
+Console.WriteLine("The length of intArray is {0}", intLength);
+
+Console.WriteLine("The first int in intArray is {0}", intArray[0]);
+
+Console.ReadKey();
+
+
+
