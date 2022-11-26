7b9531f1f88baadae284fa567782217eafd29dcf
yitengtang <104777271+yitengtang@users.noreply.github.com>
2022-11-25 20:51:19 -0500

    Add files via upload

---
+++ SumVals/SumVals/SumVals/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SumVals;
+class Program
+{
+    static int SumVals(params int[] vals)
+    {
+        int sum = 0;
+
+        foreach (int val in vals)
+        {
+            sum += val;
+        }
+        return sum;
+    }
+    static void Main(string[] args)
+    {
+        int sum = SumVals(1, 2, 3, 4, 5);
+        Console.WriteLine("The sum vals is {0}", sum);
+        Console.ReadKey();
+    }
+}
+
